0c21a669542c473319e3ef572285b6f51421dff5
fabiendaguet <daguet77@gmail.com>
2023-02-02 17:01:03 +0100

    Modification des props attendus, ajout de style conditionel en fonction du composant parent ajout d'élement html affiché en fonction de l'élément parent via des props de styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,60 @@
-const Card = props => {
+const Card = (props) => {
   return (
-    <div style={styles.card}>
-        <img style={styles.img} src={props.card.img} alt={"Carte " + props.card.name}/>
-        <h2 style={styles.name}>{props.card.name}</h2>
+    <div style={{ ...styles.card, ...props.style?.sellContainer }}>
+      <div style={props.style?.sellCard}>
+        <img style={styles.img} src={props.img} alt={"Carte " + props.name} />
+        <h2 style={styles.name}>{props.name}</h2>
+      </div>
+      <div style={{ ...styles.sellInfo, ...props.style?.sellInfo }}>
+        <div>
+          <div style={props.style?.sellInfoElement}>
+            Vendeur : {props.infoUser?.userName}
+          </div>
+          <div style={props.style?.sellInfoElement}>
+            État : {props.infoCard?.quality}
+          </div>
+          <div>Prix : {props.infoCard?.price}€</div>
+        </div>
+        <button style={styles.button}>Ajouter au panier</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const styles = {
-    card: {
-        padding: '.5rem',
-        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 0px 8px',
-        borderRadius: '3%',
-        gap: '.5rem',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    },
-    img: {
-        width: '100%',
-        borderRadius: '3%',
-    },
-    name: {
-        fontSize: '14px',
-        margin: 0,
-        padding: 0,
-        textAlign: 'center',
-    }
-}
+  card: {
+    padding: ".5rem",
+    boxShadow: "rgba(0, 0, 0, 0.24) 0px 0px 8px",
+    borderRadius: "3%",
+    gap: ".5rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  img: {
+    width: "100%",
+    borderRadius: "3%",
+  },
+  name: {
+    fontSize: "14px",
+    margin: 0,
+    padding: 0,
+    textAlign: "center",
+  },
 
-export default Card
+  sellInfo: {
+    display: "none",
+  },
+
+  button: {
+    width: "100%",
+    padding: ".5rem 1rem",
+    color: "#fff",
+    backgroundColor: "rgb(100, 106, 234)",
+    border: "none",
+    borderRadius: "10px",
+  },
+};
+
+export default Card;
